0acc1d369f862b8c90a0e8669ab797fcf720eb1c
djaffe <danieljaffe@mail.tau.ac.il>
2023-04-23 17:52:51 +0300

    Adjust urls.py file with only needed urls

--- glpi_scripts/urls.py
+++ glpi_scripts/urls.py
@@ -8,20 +8,22 @@ class Urls:
         netloc = p.netloc or p.path
         path = p.path if p.netloc else ""
         p = urllib.parse.ParseResult("https", netloc, path, *p[3:])
+
         self.HOME_URL = p.geturl()
         self.BASE_URL = self.HOME_URL + "/apirest.php/"
+
+        # session urls
         self.INIT_URL = self.BASE_URL + "initSession"
         self.KILL_URL = self.BASE_URL + "killSession"
 
-        self.SEARCH_URL = self.BASE_URL + "search/"
-        self.SEARCH_COMPUTER_URL = self.SEARCH_URL + "Computer/"
-
+        # computer url
         self.COMPUTER_URL = self.BASE_URL + "Computer/"
+
+        # reservation url
         self.RESERVATION = self.BASE_URL + "Reservation/"
 
-        self.SEARCH_OPTIONS = self.BASE_URL + "listSearchOptions/"
-        self.SEARCH_OPTIONS_COMPUTER = self.SEARCH_OPTIONS + "Computer/"
-
+        # user url
         self.USER_URL = self.BASE_URL + "User/"
 
+        # reservation item url
         self.RESERVATION_ITEM = self.BASE_URL + "ReservationItem/"
